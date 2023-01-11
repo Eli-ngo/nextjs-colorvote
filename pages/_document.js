a23844d1dbb2dc2a553089439778b77d3da25259
Elisabeth Ngo <78852168+Eli-ngo@users.noreply.github.com>
2023-01-11 15:45:50 +0100

    front guest portal + instructions

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,12 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#fff" />
+        
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;600;700&display=swap" rel="stylesheet"/>
+        
       </Head>
       <body>
         <Main />
---
+++ pages/item/[id].js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const ItemDetails = () => {
+
+    return(
+        <>
+            <Head>
+                <title>Colorvote | Portail invité</title>
+            </Head>
+        </>
+    )
+}
+
+export default ItemDetails
