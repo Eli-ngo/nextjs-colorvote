53631128cdf50bc59e4ae8f3d5f0a2610c65f331
Sarah HITIER <sarah.hitier@sfr.fr>
2023-01-11 15:55:57 +0100

    feat: Theme colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+
+import { COLORS } from "../theme/colors";
+const theme = {
+    colors: COLORS
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
---
+++ theme/colors.js
@@ -0,0 +1,37 @@
+// import { createGlobalStyle } from "styled-components";
+
+// // create a global style component
+// export const GlobalStyle = createGlobalStyle`
+//   :root {
+//     --primary-color: #1363DA;
+//     --background-color: #F4F4F4;
+//     --text-color: #1E1E1E;
+
+//     // Pickers :
+//     --darkGreen: #055B0E;
+//     --green: #B5D64E;
+//     --orange: #FF9900;
+//     --red: #FF0000;
+//     --darkRed: #680000;
+//     --white: #E0E0E0;
+//     --black: #1E1E1E;
+//   }
+// `;
+
+export const COLORS = {
+    primary: "#1363DA",
+    background: "#F4F4F4",
+    text: "#1E1E1E",
+
+    // Pickers :
+    pickers: {
+        default: "#055B0E",
+        darkGreen: "#055B0E",
+        green: "#B5D64E",
+        orange: "#FF9900",
+        red: "#FF0000",
+        darkRed: "#680000",
+        white: "#E0E0E0",
+        black: "#1E1E1E"
+    }
+};
